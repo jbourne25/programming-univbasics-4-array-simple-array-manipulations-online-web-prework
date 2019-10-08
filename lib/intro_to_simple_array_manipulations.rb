12301d7afddd65e0fccd9cacd51b5ba5dab34324
Jeremy Bourne <jbourne25@yahoo.com>
2019-10-08 01:50:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,7 @@ def using_push(colors_in_the_rainbow, next_color)
   colors_in_the_rainbow.push("violet")
 end
 
-def using_unshift(bouroughs_in_nyc, new_neighborhood)
+def using_unshift(bouroughs_in_nyc)
   new_neighborhood.first("Staten Island")
 end
 
