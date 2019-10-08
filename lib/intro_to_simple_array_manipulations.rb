f66d7d40c7549f3e27437294f0801ea30455e3a9
Jeremy Bourne <jbourne25@yahoo.com>
2019-10-08 04:20:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,8 +30,8 @@ def using_insert(list_of_programming_languages, another_language)
   list_of_programming_languages.insert(4, another_language)
 end
 
-def using_uniq(haircuts, new_array)
-  haircuts.uniq("bob")
+def using_uniq(haircuts)
+  haircuts.uniq
   new_array
 end
 
