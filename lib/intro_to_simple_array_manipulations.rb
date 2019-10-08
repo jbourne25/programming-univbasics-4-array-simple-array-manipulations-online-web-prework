a6130a257275a1d6f5983c274d1a70c023f4f926
Jeremy Bourne <jbourne25@yahoo.com>
2019-10-08 02:20:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,8 +6,7 @@ def using_unshift(bouroughs_in_nyc, new_neighborhood)
   bouroughs_in_nyc.unshift(new_neighborhood)
 end
 
-def using_pop(continents, deleted_string)
-  deleted_string = "Antarctica"
+def using_pop(continents,deleted_string)
   continents.pop(deleted_string)
 end
   
