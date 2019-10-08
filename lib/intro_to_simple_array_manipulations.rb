6979fbecada9d569ea75093bcd1604c136955445
Jeremy Bourne <jbourne25@yahoo.com>
2019-10-08 04:38:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,7 +42,7 @@ def using_delete(instructors, no_offense_steven)
   instructors.delete(no_offense_steven)
 end
 
-def using_delete_at(famous_robots)
+def using_delete_at(famous_robots, deleted_robot)
   famous_robots.delete(2)
 end
 
