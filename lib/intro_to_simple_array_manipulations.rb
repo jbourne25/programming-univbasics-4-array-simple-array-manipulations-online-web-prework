30e484ac024b18320521dfdb81fc81bc6e493bff
Jeremy Bourne <jbourne25@yahoo.com>
2019-10-08 01:33:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,8 @@ def using_push(colors_in_the_rainbow, next_color)
   colors_in_the_rainbow.push("violet")
 end
 
-def using_unshift()
-   .first(c)
+def using_unshift(bouroughs_in_nyc)
+   .first(new.)
 end
 
   
