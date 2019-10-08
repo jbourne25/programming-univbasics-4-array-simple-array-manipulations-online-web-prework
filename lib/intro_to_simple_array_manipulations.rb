9b6f695de220dc0b5d39261fc9d318622bd2b324
Jeremy Bourne <jbourne25@yahoo.com>
2019-10-08 01:38:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,6 +3,7 @@ def using_push(colors_in_the_rainbow, next_color)
 end
 
 def using_unshift(bouroughs_in_nyc, new_neighborhood)
+  new_neighborhood = "Staten Island"
   bouroughs_in_nyc.first(new_neighborhood)
 end
 
