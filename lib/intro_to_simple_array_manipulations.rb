442a0c1cddb6f89555437a183734776907353ca1
Jeremy Bourne <jbourne25@yahoo.com>
2019-10-08 03:35:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,7 +26,9 @@ def using_concat(my_favorite_things, all_my_favs)
   my_favorite_things.concat(all_my_favs)
 end
 
-def using_insert(list_of_programming_languages, )
+def using_insert(list_of_programming_languages, another_language)
+  list_of_programming_languages.insert(4)
+end
 
 
 
