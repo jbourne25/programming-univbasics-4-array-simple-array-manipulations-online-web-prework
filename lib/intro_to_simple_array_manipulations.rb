e11788601393801714575f06628a397d06fb9d5a
Jeremy Bourne <jbourne25@yahoo.com>
2019-10-08 01:57:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,8 +3,7 @@ def using_push(colors_in_the_rainbow, next_color)
 end
 
 def using_unshift(bouroughs_in_nyc, new_neighborhood)
-  bouroughs_in_nyc.first(new_neighborhood)
-  puts new_neighborhood
+  bouroughs_in_nyc.first("Staten Island")
 end
 
   
